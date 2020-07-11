6a6955f66a29c912720780a1df0e590d2086e4e8
miguepity <miguel.ardonenriquez>
2020-07-11 00:27:31 -0600

    adding all days of a month

--- calendar-app/src/components/skeleton.js
+++ calendar-app/src/components/skeleton.js
@@ -39,10 +39,50 @@ const Calendar = () => {
         return <div className="days row">{days}</div>
     }
 
+    const oneDay = () =>{
+        const firstDayMonth = moment(currentDate).startOf('month')
+        const lastDayMonth = moment(currentDate).endOf('month')
+        const firstDate = moment(firstDayMonth).startOf('week')
+        const lastDate = moment(lastDayMonth).endOf('week')
+        const row = []
+
+        let days = []
+        let day = firstDate
+        let dateFormat = ""
+
+        while(day <= lastDate){
+            for(let i =0; i < 7; i++){
+                dateFormat = moment(day).format('D')
+                const sameDay = day
+                days.push(
+                    <div className={`col oneD ${!moment().isSame(day,firstDayMonth)
+                    ? "disable" : moment().isSame(day, selectedDate)
+                    ? "selected": ""}`}
+                    key={day}
+                    onClick={() => onClickDate(moment(sameDay))}>
+                        <span className="number">{dateFormat}</span>
+                    </div>
+                )
+                day = moment(day).add(1,'d')
+            }
+            row.push(
+                <div className="row" kewy={day}>{days}</div>
+            )
+            days = []
+        }
+        return <div className="body">{row}</div>
+    }
+
+    const onClickDate = day =>{
+        setSelectedDate(day)
+        alert(selectedDate)
+    }
+
     return (
         <div className="calendar">
             <div>{header()}</div>
             <div>{weekDays()}</div>
+            <div>{oneDay()}</div>
         </div>
     )
 }
